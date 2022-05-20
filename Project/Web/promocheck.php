aeb4b3343ca2df53ab3b24054a8f78ac78ea30c3
kayceetann <kayceetan96@gmail.com>
2022-05-21 01:51:56 +0800

    Create promocheck.php

---
+++ Project/Web/promocheck.php
@@ -0,0 +1,42 @@
+
+<?php
+session_start();
+include ("function.php");
+
+$servername="localhost";
+$username="root";
+$serverpw="";
+$dbname="restaurant";
+$dbtable="coupon";
+
+$conn = new mysqli($servername, $username, $serverpw, $dbname);
+if ($conn->connect_error) { die("connection failed"); }
+//print_r($menuArr); //test array set
+
+if(isSet($_POST['coupon']))
+{
+	$coupon = $_POST['coupon'];
+	$coupon = mysqli_real_escape_string($conn,$_POST["coupon"]); //escapes special characters in a string
+	$coupon = strip_tag($coupon); //function that take out unneccessary characters in a string
+
+	$query=$conn->prepare("select `COUPON ID`, `DISCOUNT RATE` from coupon where `coupon code` = ? "); //check if user exists in database
+	$query->bind_param('s',$coupon); //bind parameter
+	$query->execute(); //execute query
+	$query->bind_result($couponid, $discRate);
+	$query->fetch(); //fetch query
+	$query->close(); //close query
+	if($couponid !="")
+	{
+		$_SESSION['discRate'] = $discRate;
+		$_SESSION['couponid'] = $couponid;
+		die('Promo code is applied.');
+
+	}
+	else
+	{
+		die('Invalid promo code.');
+	}
+}
+
+?>
+
